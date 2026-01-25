eb9773583e1e788e5ea2f4bf6eefde2762e17fe9
parker gibbons <parkerjgibbons@gmail.com>
2026-01-25 15:54:03 -0800

    integrate Vercel Analytics for improved tracking

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -11,5 +12,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
